ada26b83ca02c852d47df02729cc574b7823f1db
sungsu9022 <sungsu9022@naver.com>
2026-02-08 15:39:16 +0900

    사진 등 리소스 업데이트

--- src/common/viewerImages.js
+++ src/common/viewerImages.js
@@ -1,26 +1,15 @@
 export default {
 	images : [
-		"wedding/gallery/0_h.jpg",
-		"wedding/gallery/1_h.jpg",
-		"wedding/gallery/2_h.jpg",
+		"wedding/gallery/0.jpg",
+		"wedding/gallery/1.jpg",
+		"wedding/gallery/2.jpg",
 		"wedding/gallery/3.jpg",
 		"wedding/gallery/4.jpg",
 		"wedding/gallery/5.jpg",
-		"wedding/gallery/6.jpg",
-		"wedding/gallery/7.jpg",
-		"wedding/gallery/8.jpg",
+		"wedding/gallery/6_h.jpg",
+		"wedding/gallery/7_h.jpg",
+		"wedding/gallery/8_h.jpg",
 		"wedding/gallery/9.jpg",
-		"wedding/gallery/10.jpg",
-		"wedding/gallery/11.jpg",
-		"wedding/gallery/12.jpg",
-		"wedding/gallery/13.jpg",
-		"wedding/gallery/14.jpg",
-		"wedding/gallery/15_h.jpg",
-		"wedding/gallery/16_h.jpg",
-		"wedding/gallery/17_h.jpg",
-		"wedding/gallery/18.jpg",
-		"wedding/gallery/19.jpg",
-		"wedding/gallery/20.jpg",
 	]
 
 }
